55d92198241aefce756e1884f267d8a76cfe1b5d
dehqalbi <dehyasvibe@gmail.com>
2023-06-07 20:06:17 +0700

    add login form

--- fe-news/src/components/login.js
+++ fe-news/src/components/login.js
@@ -1,5 +1,45 @@
+import { useState } from "react";
+
 export default function Login() {
-    return (
-        <div>Login page</div>
-    )
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const submitForm = () => {
+    console.log(email + " " + password);
+  };
+  return (
+    <div className="row justify-content-center pt-5">
+      <div className="col-sm-6">
+        <div className="card p-4">
+          <div className="form-group">
+            <label>Email address:</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+              id="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password:</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              id="pwd"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button
+            type="button"
+            onClick={submitForm}
+            className="btn btn-primary mt-4"
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
